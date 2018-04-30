52e392ecfaf7778f6421ea954fea6833176477a6
TaniaTru <tatiana.trukhachova@gmail.com>
2018-04-30 14:34:25 +0300

    Added Pagination

--- MasterpieceStore/MasterpieceStore.WebUI/Controllers/ProductController.cs
+++ MasterpieceStore/MasterpieceStore.WebUI/Controllers/ProductController.cs
@@ -10,15 +10,19 @@ namespace MasterpieceStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        public int PageSize = 4;
         private IProductRepository repository;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+            public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize));
         }
     }
 }
