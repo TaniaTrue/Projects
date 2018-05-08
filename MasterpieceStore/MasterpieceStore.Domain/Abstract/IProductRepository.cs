d1c7733ce94ff8b2f0d843b0073b6caa7408ebfd
TaniaTru <tatiana.trukhachova@gmail.com>
2018-05-08 10:53:34 +0300

    Updated the Product Repository

--- MasterpieceStore/MasterpieceStore.Domain/Abstract/IProductRepository.cs
+++ MasterpieceStore/MasterpieceStore.Domain/Abstract/IProductRepository.cs
@@ -10,5 +10,6 @@ namespace MasterpieceStore.Domain.Abstract
   public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+        void SaveProduct(Product product);
     }
 }
