a915d75d4625bb8db98f970b1e7c008f9cb22fbb
TaniaTru <tatiana.trukhachova@gmail.com>
2018-05-08 11:08:18 +0300

    Applied Validation Attributes to the Product.cs File

--- MasterpieceStore/MasterpieceStore.Domain/Entities/Product.cs
+++ MasterpieceStore/MasterpieceStore.Domain/Entities/Product.cs
@@ -13,10 +13,15 @@ namespace MasterpieceStore.Domain.Entities
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal? MinPrice { get; set; }
         public short? Created { get; set; }
 
