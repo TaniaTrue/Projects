0f12cb75a393baede725d97c5f68ab190533faf0
TaniaTru <tatiana.trukhachova@gmail.com>
2018-05-08 09:30:23 +0300

    I don't knowhow to avoid it

---
+++ QuizTests/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuizTests
+{
+    class Class1
+    {
+        private static readonly object Lock1 = new object();
+        private static readonly object Lock2 = new object();
+
+        public static void Func1()
+        {
+            lock (Lock1)
+            {
+                Thread.Sleep(1000);
+                lock(Lock2)
+                    Console.WriteLine("ABC");
+            }
+        }
+
+        public static void Func2()
+        {
+            lock (Lock2)
+            {
+                Thread.Sleep(1000);
+                lock (Lock1)
+                    Console.WriteLine("DEF");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+
+            //Console.Write("Begin");
+
+            //var thr1 = new Thread(Func1);
+            //var thr2 = new Thread(Func2);
+
+            //thr1.Start();
+            //thr2.Start();
+            //Console.Write("End");
+
+            Counter c1 = new Counter();
+            c1.GetCount();
+
+            Counter c2 = new Counter();
+            Console.WriteLine( c2.GetCount());
+
+            Console.ReadLine();
+        }
+
+        class Counter
+        {
+            public static int count = 0;
+
+            public int GetCount()
+            {
+                return ++count;
+            }
+        }
+
+    }
+}
+
+
+//var result = "";
+//var arg1 = "short string";
+//var arg2 = "long string";
+
+//result = arg2.Length > arg2.Length
+//    ? arg2[0] < arg1[0]
+//    ? arg2[arg2.Length - 1] == arg1[arg1.Length - 1]
+//    ? arg1
+//    : arg2
+//    : arg1.ToUpper()
+//    : arg2.ToUpper(); LONG STRING
+
+
+
+
+//------------------------------------------
+
+
+//class FooA
+//{
+//    public void Method()
+//    {
+//        Console.WriteLine("A");
+//    }
+//}
+//class FooB : FooA
+//{
+//    new void Method()
+//    {
+//        Console.WriteLine("B");
+//    }
+//}
+
+
+//var fooB = new FooB();
+//fooB.Method();
+
+//------------------------------------------
--- QuizTests/Program.cs
+++ QuizTests/Program.cs
@@ -1,155 +1,166 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿////using System;
+////using System.Collections;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
+////using System.Collections.ObjectModel;
 
-using ByteCollection1 = System.Collections.ObjectModel.Collection<byte>;
-
-
-
-namespace QuizTests
-{
-     interface Popo
-    {
-         void Kiki();
-    }
-    internal sealed class Some<T>
-    {
-
-    }
-    public class Foo
-    {
-        private int[] a = new int[10];
-        private Dictionary<string, int> b;
-        private int[] c = new int[100];
-
-        public int this[int i]
-        {
-            get { return a[i]; }
-            set { a[i] = value; }
-        }
-
-        public int this[string id]
-        {
-            get { return b[id]; }
-            set { b[id] = value; }
-        }
-
-        public int this[int i, int j]
-        {
-            get { return c[i * 10 + j]; }
-            set { c[i * 10 + j] = value; }
-        }
-    }
+////using ByteCollection1 = System.Collections.ObjectModel.Collection<byte>;
 
 
-    class Program
-    {
-        static IEnumerable<string> Get()
-        {
-            yield
-                return "1";
-            Console.WriteLine("2");
-            yield
-                return "3";
-        }
-        static void  Main(string[] args)
-        {
+
+////namespace QuizTests
+////{
+////    interface Popo
+////    {
+////        void Kiki();
+////    }
+////    internal sealed class Some<T>
+////    {
+
+////    }
+////    public class Foo
+////    {
+////        private int[] a = new int[10];
+////        private Dictionary<string, int> b;
+////        private int[] c = new int[100];
+
+////        public int this[int i]
+////        {
+////            get { return a[i]; }
+////            set { a[i] = value; }
+////        }
+
+////        public int this[string id]
+////        {
+////            get { return b[id]; }
+////            set { b[id] = value; }
+////        }
+
+////        public int this[int i, int j]
+////        {
+////            get { return c[i * 10 + j]; }
+////            set { c[i * 10 + j] = value; }
+////        }
+////    }
 
 
-            //Type t1 = typeof(System.Int32);
-            //Type t2 = Type.GetType("System.Int32");
-            //Type t3 = Type.GetType(System.Int32);
-            //Type t4 = System.Int32.GetType();
-            //int x = 5;
-            //int y = 8;
-            //(x==5) && ((y == 8) && !(x==y))
-            //    { }
+////    class Program
+////    {
+////        static IEnumerable<string> Get()
+////        {
+////            yield
+////                return "1";
+////            Console.WriteLine("2");
+////            yield
+////                return "3";
+////        }
 
-            Decimal x = Decimal.MaxValue;
-
-            unchecked
-            {
-                x++;
-            }
-
-            if (x == Decimal.MinValue)
-            {
-                Console.WriteLine("MinValue");
-            }
-            else if (x == Decimal.MaxValue)
-            {
-                Console.WriteLine("MaxValue");
-            }
-            else
-            {
-                Console.WriteLine("po");
-            }
-            Console.ReadLine();
-        }
-
-        class Name
-        {
-            public string A
-            {
-                private set;
-                get;
-            }
-
-            //private string B
-            //{
-            //    public set;
-            //    public get;
-            //}
-
-            private string x;
-            public string C
-            {
-                get { return x.ToString(); }
-            }
-
-            //public string D
-            //{
-            //    set { x = value; }
-            //    private get;
-            //}
-        }
-
-        public class Foo
-        {
-            private int a;
-            private int c;
-
-            public int A
-            {
-                get { return a; }
-                set { a = value; }
-            }
-
-            public int B
-            {
-                get { return 0; }
-            }
+////        public static void Method(ref int x, out int y)
+////        {
+////            y = 7;
+////            x = 6;
+////        }
+////        static void Main(string[] args)
+////        {
+////            int x = 5;
+////            Method(ref x, out x);
+////            Console.WriteLine(x);
 
 
-            public int C
-            {
-                set {c =value; }
-            }
 
-            //protected int D { get; set; }
-            //protected int E { get; private set; }
-            //protected int F { public get; set; }
+////            Type t1 = typeof(System.Int32);
+////            Type t2 = Type.GetType("System.Int32");
+////            Type t3 = Type.GetType(System.Int32);
+////            Type t4 = System.Int32.GetType();
 
-        }
+////            int x = 5;
+////            int y = 8;
+////            if (x == 5) && ((y == 8) && !(x == y))
+////                { }
 
-        class A
-        {
-            class B : A
-            { }
-        }
-    }
-}
+////            Decimal x = Decimal.MaxValue;
+
+////            unchecked
+////            {
+////                x++;
+////            }
+
+////            if (x == Decimal.MinValue)
+////            {
+////                Console.WriteLine("MinValue");
+////            }
+////            else if (x == Decimal.MaxValue)
+////            {
+////                Console.WriteLine("MaxValue");
+////            }
+////            else
+////            {
+////                Console.WriteLine("po");
+////            }
+////            Console.ReadLine();
+////        }
+
+////        class Name
+////        {
+////            public string A
+////            {
+////                private set;
+////                get;
+////            }
+
+////            private string B
+////            {
+////                public set;
+////                public get;
+////            }
+
+////            private string x;
+////            public string C
+////            {
+////                get { return x.ToString(); }
+////            }
+
+////            public string D
+////            {
+////                set { x = value; }
+////                private get;
+////            }
+////        }
+
+////        public class Foo
+////        {
+////            private int a;
+////            private int c;
+
+////            public int A
+////            {
+////                get { return a; }
+////                set { a = value; }
+////            }
+
+////            public int B
+////            {
+////                get { return 0; }
+////            }
+
+
+////            public int C
+////            {
+////                set { c = value; }
+////            }
+
+////            protected int D { get; set; }
+////            protected int E { get; private set; }
+////            protected int F { public get; set; }
+
+////        }
+
+////        class A
+////        {
+////            class B : A
+////            { }
+////        }
+////    }
+////}
