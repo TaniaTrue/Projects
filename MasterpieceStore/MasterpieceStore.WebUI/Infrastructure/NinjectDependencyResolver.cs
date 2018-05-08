092b7aab7125d1ecc4ce7ec509577779a98d0551
TaniaTru <tatiana.trukhachova@gmail.com>
2018-05-08 09:57:01 +0300

    Edited the NinjectDependencyResolver to register an implementation of IOrderProcessor

--- MasterpieceStore/MasterpieceStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ MasterpieceStore/MasterpieceStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,6 +3,7 @@ using MasterpieceStore.Domain.Concrete;
 using Ninject;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,6 +29,15 @@ namespace MasterpieceStore.WebUI.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+ .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+            .WithConstructorArgument("settings", emailSettings);
         }
+
     }
 }
