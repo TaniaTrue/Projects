02a0a369b492eb2deaacb08925eced95229f7683
TaniaTrue <tatiana.trukhachova@gmail.com>
2018-07-09 13:37:35 +0300

    support for Roles

--- MasterpieceStore/MasterpieceStore.WebUI/App_Start/IdentityConfig.cs
+++ MasterpieceStore/MasterpieceStore.WebUI/App_Start/IdentityConfig.cs
@@ -16,6 +16,7 @@ namespace MasterpieceStore.WebUI.App_Start
         {
             app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
             app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<AppRoleManager>(AppRoleManager.Create);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
