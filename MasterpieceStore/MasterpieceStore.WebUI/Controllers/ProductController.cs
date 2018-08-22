338b76290a5f5a18dbe392b6cc9cebe31fa7fbe1
TaniaTrue <tatiana.trukhachova@gmail.com>
2018-08-22 17:01:36 +0300

    Replaced methods

--- MasterpieceStore/MasterpieceStore.WebUI/Controllers/ProductController.cs
+++ MasterpieceStore/MasterpieceStore.WebUI/Controllers/ProductController.cs
@@ -19,6 +19,11 @@ namespace MasterpieceStore.WebUI.Controllers
             this.repository = productRepository;
         }
 
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
         public ViewResult List(int page = 1)
         {
                 ProductsListViewModel model = new ProductsListViewModel()
@@ -48,6 +53,51 @@ namespace MasterpieceStore.WebUI.Controllers
             {
                 return null;
             }
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+
+                }
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //there is something wrong with the data values
+                return View(product);
+            }
+        }
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int productID)
+        {
+            Product deletedProduct = repository.DeleteProduct(productID);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted",
+ deletedProduct.Name);
+            }
+            return RedirectToAction("Index");
         }
 
     }
